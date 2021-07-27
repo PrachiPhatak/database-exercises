5cb5403aabab39546fcd471f9d7e84b09de3adf0
PrachiPhatak <phatak.prachi@gmail.com>
2021-07-27 12:15:06 -0500

    Add delete exercise

---
+++ delete_exercises.sql
@@ -0,0 +1,34 @@
+use codeup_test_db;
+
+select 'Albums released after 1991' as Info;
+
+delete
+from albums
+where release_date > 1991;
+
+select *
+from albums
+where release_date > 1991;
+# ----------------------------------------------------------------------
+
+select 'Albums with the genre ''disco''' as Info;
+
+delete
+from albums
+where genre = 'disco';
+
+select *
+from albums
+where release_date > 1991;
+# ----------------------------------------------------------------------
+
+select 'Albums by ''Whitney Houston''' as Info;
+
+delete
+from albums
+where artist = 'Whitney Houston';
+
+select *
+from albums
+where artist = 'Whitney Houston'/G;
+# ----------------------------------------------------------------------

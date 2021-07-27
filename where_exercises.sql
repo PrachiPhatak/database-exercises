2bed4db784e15845766713fa24c28243d6bac812
PrachiPhatak <phatak.prachi@gmail.com>
2021-07-27 18:59:09 -0500

    Add part two of where exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -11,3 +11,32 @@ where last_name like 'E%';
 select count(*)
 from employees
 where last_name like '%q%';
+
+# Part Two
+select *
+from employees
+where first_name = 'Irena'
+   or first_name = 'Vidya'
+   or first_name = 'Maya';
+
+select *
+from employees.employees
+where (first_name = 'Irena'
+    or first_name = 'Vidya'
+    or first_name = 'Maya')
+  And gender = 'm';
+
+select *
+from employees.employees
+where last_name like 'E%'
+   or last_name like '%E';
+
+select *
+from employees.employees
+where last_name like 'E%'
+  and last_name like '%E';
+
+select count(*)
+from employees.employees
+where last_name like '%q%'
+  and last_name not like '%qu%';
